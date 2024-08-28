20ca96df70911c3d78b5d037248552dd8c2e2d9a
Marcelo Pignataro <pignataromd@gmail.com>
2024-08-28 15:23:29 +0100

    installs dotenv and sets the appwrite linking

---
+++ types/env.d.ts
@@ -0,0 +1,9 @@
+declare module "@env" {
+	export const APPWRITE_ENDPOINT: string;
+	export const BUNDLE_ID: string;
+	export const PROJECT_ID: string;
+	export const DATABASE_ID: string;
+	export const USERCOLLECTION_ID: string;
+	export const VIDEOCOLLECTION_ID: string;
+	export const STORAGE_ID: string;
+}
