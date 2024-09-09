3328eeb2706e38988eaa76b38951106a31433556
Marcelo Pignataro <pignataromd@gmail.com>
2024-09-09 17:24:35 +0200

    New global provider improvement.

--- context/GlobalProvider.tsx
+++ context/GlobalProvider.tsx
@@ -1,4 +1,3 @@
-import { getCurrentUser } from "@/lib/appwrite";
 import {
 	createContext,
 	ReactElement,
@@ -6,29 +5,40 @@ import {
 	useEffect,
 	useState,
 } from "react";
+import { getCurrentUser } from "@/services/appwrite";
+import { loggedInUserInitState } from "./initialStates";
+import { LoggedInUser } from "@/types/context";
 
-const GlobalContext = createContext();
+const GlobalContext = createContext({
+	isLoading: true,
+	isLoggedIn: false,
+	setIsLoggedIn: (value: boolean) => undefined as void,
+	setIsLoading: (value: boolean) => undefined as void,
+	setUser: (value: LoggedInUser) => undefined as void,
+	user: loggedInUserInitState,
+});
 
 export const useGlobalContext = () => useContext(GlobalContext);
 
 export const GlobalProvider = ({ children }: { children: ReactElement }) => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [user, setUser] = useState(null);
+	const [user, setUser] = useState(loggedInUserInitState);
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		getCurrentUser()
-			.then((response) => {
-				console.log(response);
+			.then((response: any) => {
 				if (response) {
 					setIsLoggedIn(true);
 					setUser(response);
 				} else {
 					setIsLoggedIn(false);
-					setUser(null);
+					setUser(loggedInUserInitState);
 				}
 			})
-			.catch((error) => console.log(error))
+			.catch((error) => {
+				console.log("Errorr", error);
+			})
 			.finally(() => {
 				setIsLoading(false);
 			});
